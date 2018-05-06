5941c25cd7307a40c1c06d976dc7049ea22e880d
lokismk <loki_struga@hotmail.com>
2018-05-07 00:16:53 +0200

    Update and rename index.js to bot.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const commando = require('discord.js-commando');
-const bot = new commando.Client();
-
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
-
-bot.login(process.env.BOT_TOKEN);
