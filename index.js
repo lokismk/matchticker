9a4744f0bbc323984682cafda4009ab63fccbd31
lokismk <loki_struga@hotmail.com>
2018-05-06 22:29:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ bot.registry.registerGroup('random', 'Random');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login('NDQyMzk1MzA2OTgyNjM3NTc1.Dc-MYg.0mkbk9UY17HV6GJPQ89v1fO3oQA');
+bot.login(process.env.BOT_TOKEN);
