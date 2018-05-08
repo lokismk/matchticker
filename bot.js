a1f51affe3172c7e1a7597067a72592a882e485a
lokismk <loki_struga@hotmail.com>
2018-05-08 17:16:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === 'duts') {
 
-       message.reply('pong');
+       message.reply('dule');
 
        }
 
